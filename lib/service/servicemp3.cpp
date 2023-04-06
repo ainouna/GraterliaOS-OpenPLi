e2846dcbdb373bbada8d38f8b1c09cf4b02fc7f7
ainouna <fleur2003_fr@yahoo.fr>
2023-04-06 10:49:56 +0100

    Update servicemp3.cpp

--- lib/service/servicemp3.cpp
+++ lib/service/servicemp3.cpp
@@ -952,7 +952,7 @@ DEFINE_REF(eServiceMP3);
 DEFINE_REF(GstMessageContainer);
 #endif
 
-RESULT eServiceMP3::connectEvent(const Slot2<void,iPlayableService*,int> &event, ePtr<eConnection> &connection)
+RESULT eServiceMP3::connectEvent(const sigc::slot2<void,iPlayableService*,int> &event, ePtr<eConnection> &connection)
 {
 	connection = new eConnection((iPlayableService*)this, m_event.connect(event));
 #ifdef ENABLE_MEDIAFWGSTREAMER
